1f84cfcdb6f636cef588c8c81617d124faa86b60
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-07-30 09:39:27 +0530

    More about component selector.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeesComponent } from './employees/employees.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,EmployeesComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss','./external.component.scss']
 })
