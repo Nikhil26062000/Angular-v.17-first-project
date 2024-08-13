066f301fd5fd27d50c6716181655f1c4fe77a4f0
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-08-13 14:16:04 +0530

    Form and table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { EmployeesComponent } from './employees/employees.component';
 import { MainHeaderComponent } from './core/main-header/main-header.component';
 import { MainFooterComponent } from './core/main-footer/main-footer.component';
+import { TableComponent } from './shared/component/table/table.component';
+import { AddEmployeeFormComponent } from './components/add-employee-form/add-employee-form.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MainHeaderComponent,MainFooterComponent],
+  imports: [MainHeaderComponent,MainFooterComponent,TableComponent,AddEmployeeFormComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss','./external.component.scss']
 })
---
+++ src/app/components/add-employee-form/add-employee-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-employee-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-employee-form.component.html',
+  styleUrl: './add-employee-form.component.scss'
+})
+export class AddEmployeeFormComponent {
+
+}
