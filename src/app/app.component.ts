b5f9e18ef02cfa3e3da4b7c2a912844c724c3820
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-08-15 19:16:14 +0530

    fetching data from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,40 +5,48 @@ import { MainFooterComponent } from './core/main-footer/main-footer.component';
 import { TableComponent } from './shared/component/table/table.component';
 import { AddEmployeeFormComponent } from './components/add-employee-form/add-employee-form.component';
 import { AppService } from './shared/services/app.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MainHeaderComponent,MainFooterComponent,TableComponent,AddEmployeeFormComponent],
+  imports: [
+    MainHeaderComponent,
+    MainFooterComponent,
+    TableComponent,
+    AddEmployeeFormComponent,
+    HttpClientModule
+   
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss','./external.component.scss']
+  styleUrls: ['./app.component.scss', './external.component.scss'],
+  providers: [AppService],
 })
 export class AppComponent implements OnInit {
   title = 'first-project';
-  mainComponent=true
-  secondaryComponent=true
-  logoTitle : string = 'Employee Admin'
-  employeesData:any[]=[];
+  mainComponent = true;
+  secondaryComponent = true;
+  logoTitle: string = 'Employee Admin';
+  employeesData: any[] = [];
 
   ngOnInit(): void {
-      this.appService.getEmployees().subscribe({
-        next:(emp:any[])=>{
-          this.employeesData  = emp;
-        },
-        error:(err)=>{console.log(err)},
-        complete:()=>{},
-      });
-      
+    this.appService.getEmployees().subscribe({
+      next: (emp: any[]) => {
+        console.log(emp);
+        
+        this.employeesData = emp;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => {},
+    });
   }
 
-  getLang(lang : string){
+  getLang(lang: string) {
     console.log(lang);
-    
   }
 
-
-  constructor(private appService: AppService){
-
-  }
+  constructor(private appService: AppService) {}
 }
--- src/app/shared/services/app.service.ts
+++ src/app/shared/services/app.service.ts
@@ -1,79 +1,13 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { of } from "rxjs";
+import { Observable } from "rxjs";
 
-@Injectable({
-    providedIn: 'root'
-})
+@Injectable()
 export class AppService{
-    constructor(){}
+    constructor(private httpClient: HttpClient){}
 
-    getEmployees(){
-        return of([
-            {
-              id: 1,
-              first_name: 'John',
-              last_name: 'Doe',
-              email: 'john.doe@example.com',
-              phone: '123-456-7890',
-              gender: 'Male',
-              age: 28,
-              job_title: 'Software Engineer',
-              years_of_experience: 5,
-              salary: 80000,
-              department: 'Engineering'
-            },
-            {
-              id: 2,
-              first_name: 'Jane',
-              last_name: 'Smith',
-              email: 'jane.smith@example.com',
-              phone: '234-567-8901',
-              gender: 'Female',
-              age: 32,
-              job_title: 'Project Manager',
-              years_of_experience: 8,
-              salary: 95000,
-              department: 'Project Management'
-            },
-            {
-              id: 3,
-              first_name: 'Alice',
-              last_name: 'Johnson',
-              email: 'alice.johnson@example.com',
-              phone: '345-678-9012',
-              gender: 'Female',
-              age: 25,
-              job_title: 'UI/UX Designer',
-              years_of_experience: 3,
-              salary: 70000,
-              department: 'Design'
-            },
-            {
-              id: 4,
-              first_name: 'Bob',
-              last_name: 'Williams',
-              email: 'bob.williams@example.com',
-              phone: '456-789-0123',
-              gender: 'Male',
-              age: 40,
-              job_title: 'Product Manager',
-              years_of_experience: 15,
-              salary: 120000,
-              department: 'Product Management'
-            },
-            {
-              id: 5,
-              first_name: 'Maria',
-              last_name: 'Garcia',
-              email: 'maria.garcia@example.com',
-              phone: '567-890-1234',
-              gender: 'Female',
-              age: 30,
-              job_title: 'Data Analyst',
-              years_of_experience: 6,
-              salary: 85000,
-              department: 'Data Science'
-            }
-          ]);
+    getEmployees():Observable<any>{
+        const url:string = 'https://jsonplaceholder.typicode.com/users'
+        return this.httpClient.get<any>(url); 
     }
 }
